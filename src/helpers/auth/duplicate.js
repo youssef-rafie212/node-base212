828fec8d8120ea7bc392bdb645e78b1fa88a47aa
youssef-rafie212 <youssefelbosaty3@gmail.com>
2025-08-23 17:37:39 +0300

    typ

--- src/helpers/auth/duplicate.js
+++ src/helpers/auth/duplicate.js
@@ -1,17 +1,17 @@
 // checks if a field value is duplicated in the database
-const duplicate = async (model, fieldName, fieldBValue, updateId) => {
+const duplicate = async (model, fieldName, fieldValue, updateId) => {
     let user = null;
     // if the action is update then we exclude the current document from the check
     if (updateId) {
         user = await model.findOne({
-            [fieldName]: fieldBValue,
+            [fieldName]: fieldValue,
             status: { $ne: "deleted" },
             _id: { $ne: updateId },
         });
         // the action is create
     } else {
         user = await model.findOne({
-            [fieldName]: fieldBValue,
+            [fieldName]: fieldValue,
             status: { $ne: "deleted" },
         });
     }
