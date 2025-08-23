55d78cfd6caf4ca5e057af3e1677ff9bc716a8be
youssef-rafie212 <youssefelbosaty3@gmail.com>
2025-08-23 16:41:40 +0300

    rename file to match class name

--- src/utils/fileUpload/fileUpload.js
+++ src/utils/fileUpload/fileUpload.js
@@ -2,7 +2,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-import SaveImage from "../fileSave/fileSave.js";
+import SaveImage from "../saveImage/saveImage.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
